2d5b266a55c406099d6132ddfe8b8f356e3e5c0d
tutkuince <tutku.ince@outlook.com>
2019-02-13 12:08:46 +0300

    getGithubData returns userData and repoData as an Object

--- WebContent/js/async/github.js
+++ WebContent/js/async/github.js
@@ -4,8 +4,15 @@ class Github {
     }
 
     async getGithubData(username) {
-        const response = await fetch(this.url + username);
-        const data = response.json();
-        return data;
+        const responseData = await fetch(this.url + username);
+        const responseRepo = await fetch(this.url + username + "/repos");
+
+        const userData = await responseData.json();
+        const repoData = await responseRepo.json();
+
+        return {
+            user: userData,
+            repo: repoData
+        }
     }
 }
